9686b91b01ce111f7ff58da92fa0c8080ca5fc6d
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-02-16 16:53:35 +0000

    create webpack config, install babel

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+    entry: [
+        "./src/index.js"
+    ],
+    output: {
+        path: __dirname + "/dist",
+        publicPath: "/",
+        filename: "bundle.js"
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "/dist")
+        }
+    }
+}
