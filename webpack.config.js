bcaf8025b983064c5696600def04dc2130e8754e
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-02-16 17:13:59 +0000

    add eslint rules

--- webpack.config.js
+++ webpack.config.js
@@ -16,12 +16,19 @@ module.exports = {
     },
     module: {
         rules: [
-            test: /\.(.js|.jsx)$/,
-            exclude: /node_modules/,
-            use: ["babel-loader"],
+            {
+                test: /\.(.js|.jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.(.js|.jsx)$/,
+                exclude: /node_modules/,
+                use: ["eslint-loader"], 
+            }
         ]
     },
     resolve: {
-        extensions: ["js", "jsx"]
+        extensions: [".js", ".jsx"]
     }
 }
